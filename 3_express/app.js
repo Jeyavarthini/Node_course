1ddfc006bc2ff48be869ebf9c1205f67956bb3f8
Jeyavarthini <jeyavarthiniravi@gmail.com>
2023-08-22 16:14:29 +0530

    second commit

--- 3_express/app.js
+++ 3_express/app.js
@@ -3,4 +3,7 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send('Hello')
 })
+app.get('/about',(req,res)=>{
+    res.send('We create Impact')
+})
 app.listen(3000,()=>console.log('Port is running'))
